2252a1f3eadd77da5d7a282f81aa5f0625fa1847
iharshchauhan <50614450+iharshchauhan@users.noreply.github.com>
2024-03-01 00:47:50 +0530

    Update Challenges.tsx

--- app/_sections/Challenges.tsx
+++ app/_sections/Challenges.tsx
@@ -13,12 +13,10 @@ export const ChallengesSection = () => {
     <div className="relative mb-16 md:mb-32">
       <div className="px-container mx-auto max-w-container">
         <h2 className="mb-5 mt-4 text-center font-accent text-4xl font-medium md:text-5xl">
-          Why Embracing My Vision Elevates Your Product&apos;s Soul
+          
         </h2>
         <p className="mb-10 mt-5 text-center text-xl sm:mb-20 md:text-2xl">
-          Delve into the philosophy of design through my eyes, where each creation is not just built, but meticulously crafted with soulful purpose. 
-          In choosing to work with me, you&apos;re not just hiring a designer; you&apos;re embracing a unique vision that intertwines empathy with innovation, 
-          ensuring your product meets functional needs and speaks to the heart of the user experience.
+          Why embracing my Vision elevates your Product&apos;s Soul?
         </p>
         <div className="grid grid-cols-1 justify-items-center gap-x-10 gap-y-10 sm:grid-cols-2 sm:gap-x-16 sm:gap-y-16 lg:grid-cols-3">
           {challenges.map(challenge => {
@@ -41,30 +39,30 @@ const challenges = [
   {
     Icon: SpeedingClock,
     title:
-      "Multidisciplinary Approach: I bring a holistic view to product design, combining insights from various fields to create innovative solutions that meet users&apos; needs in unique and impactful ways.",
+      "I bring a holistic view to product design, combining insights from various fields to create innovative solutions that meet users&apos; needs in unique and impactful ways.",
   },
   {
     Icon: Cycle,
     title:
-      'Expertise in User-Centered Design: My deep understanding of user behavior ensures that I design solutions that are not just aesthetically pleasing but also incredibly intuitive and satisfying for users.',
+      'My deep understanding of user behavior ensures that I design solutions that are not just aesthetically pleasing but also incredibly intuitive and satisfying for users.',
   },
   {
     Icon: ShakeHands,
     title:
-      "Proven Experience with Top-Tier Backers: With the support of established partners like Y Combinator and Livespace, my work has a seal of approval from some of the industry&apos;s best, showcasing my ability to deliver at the highest level.",
+      "With the support of established partners like Y Combinator and Livespace, my work has a seal of approval from some of the industry&apos;s best, showcasing my ability to deliver at the highest level.",
   },
   {
     Icon: PuzzlePiece,
     title:
-      'Advanced Tool Proficiency: From design (Adobe Illustrator, Figma, Miro) to research (Notion, Airtable, Hotjar) and prototyping (Framer, Protopie, Webflow), my skill set covers the full spectrum of product development.',
+      'From design (Adobe Illustrator, Figma, Miro) to research (Notion, Airtable, Hotjar) and prototyping (Framer, Protopie, Webflow), my skill set covers the full spectrum of product development.',
   },
   {
     Icon: Money,
-    title: 'Design System Expert: With my skillful use of Webflow to establish comprehensive design systems, I ensure consistency and scalability that contribute to a cohesive user experience across all product stages.',
+    title: 'With my skillful use of Webflow to establish comprehensive design systems, I ensure consistency and scalability that contribute to a cohesive user experience across all product stages.',
   },
   {
     Icon: Growth,
     title:
-      'Agile Project Leadership: I am adept at leading cross-functional teams with agility using tools like Jira and Microsoft Clarity, ensuring swift and effective execution of project milestones.',
+      'I am adept at leading cross-functional teams with agility using tools like Jira and Microsoft Clarity, ensuring swift and effective execution of project milestones.',
   },
 ];

fbb0072edae1f4ef18e7afaa79a90c55238a2273
iharshchauhan <50614450+iharshchauhan@users.noreply.github.com>
2024-02-29 23:01:30 +0530

    Update Challenges.tsx

--- app/_sections/Challenges.tsx
+++ app/_sections/Challenges.tsx
@@ -13,11 +13,11 @@ export const ChallengesSection = () => {
     <div className="relative mb-16 md:mb-32">
       <div className="px-container mx-auto max-w-container">
         <h2 className="mb-5 mt-4 text-center font-accent text-4xl font-medium md:text-5xl">
-         Why Embracing My Vision Elevates Your Product's Soul
+          Why Embracing My Vision Elevates Your Product&apos;s Soul
         </h2>
         <p className="mb-10 mt-5 text-center text-xl sm:mb-20 md:text-2xl">
           Delve into the philosophy of design through my eyes, where each creation is not just built, but meticulously crafted with soulful purpose. 
-          In choosing to work with me, you're not just hiring a designer; you're embracing a unique vision that intertwines empathy with innovation, 
+          In choosing to work with me, you&apos;re not just hiring a designer; you&apos;re embracing a unique vision that intertwines empathy with innovation, 
           ensuring your product meets functional needs and speaks to the heart of the user experience.
         </p>
         <div className="grid grid-cols-1 justify-items-center gap-x-10 gap-y-10 sm:grid-cols-2 sm:gap-x-16 sm:gap-y-16 lg:grid-cols-3">
@@ -41,7 +41,7 @@ const challenges = [
   {
     Icon: SpeedingClock,
     title:
-      "Multidisciplinary Approach: I bring a holistic view to product design, combining insights from various fields to create innovative solutions that meet users' needs in unique and impactful ways.",
+      "Multidisciplinary Approach: I bring a holistic view to product design, combining insights from various fields to create innovative solutions that meet users&apos; needs in unique and impactful ways.",
   },
   {
     Icon: Cycle,
@@ -51,7 +51,7 @@ const challenges = [
   {
     Icon: ShakeHands,
     title:
-      "Proven Experience with Top-Tier Backers: With the support of established partners like Y Combinator and Livespace, my work has a seal of approval from some of the industry's best, showcasing my ability to deliver at the highest level.",
+      "Proven Experience with Top-Tier Backers: With the support of established partners like Y Combinator and Livespace, my work has a seal of approval from some of the industry&apos;s best, showcasing my ability to deliver at the highest level.",
   },
   {
     Icon: PuzzlePiece,
@@ -65,6 +65,6 @@ const challenges = [
   {
     Icon: Growth,
     title:
-      'Agile Project Leadership: I’m adept at leading cross-functional teams with agility using tools like Jira and Microsoft Clarity, ensuring swift and effective execution of project milestones.',
+      'Agile Project Leadership: I am adept at leading cross-functional teams with agility using tools like Jira and Microsoft Clarity, ensuring swift and effective execution of project milestones.',
   },
 ];
